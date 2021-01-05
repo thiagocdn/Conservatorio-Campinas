177f5270fd27113bcb9323262c4afb8027117051
Thiago Cedran <thiagocdn@gmail.com>
2021-01-04 22:45:19 -0300

    Started our history

--- src/pages/nossa_historia/index.tsx
+++ src/pages/nossa_historia/index.tsx
@@ -1,28 +1,44 @@
 import { motion } from "framer-motion";
-import Link from "next/link";
 
-interface CardProps {
-  href: string;
-  imgSrc: string;
-  layoutId: string;
+interface ParagraphProps {
+  text: string;
 }
 
-const Card: React.FC<CardProps> = ({ href, imgSrc, layoutId }) => {
-  return (
-    <Link href={href}>
-      <div className="relative h-20 md:h-40 bg-gray-200 rounded-xl cursor-pointer hover:bg-gray-50 transition">
-        <motion.img src={imgSrc} className="w-full h-full" layoutId={layoutId} />
-      </div>
-    </Link>
+const Paragraph: React.FC<ParagraphProps> = ({ text }) => {
+  return(
+    <p className="w-full text-white p-4 text-justify" style={{textIndent: '20px'}}>
+      {text}
+    </p>
   )
 }
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl sm:text-6xl lg:text-7xl mt-10 md:mt-24 leading-none font-extrabold tracking-tight text-gray-900">
-        Nossa Historia
-      </h1>
+      <motion.div
+        className="w-full bg-gray-100 bg-opacity-10"
+        initial={{ height: 0 }}
+        animate={{ height: 80 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20
+        }}
+      >
+        <h1 className="px-4 py-2 text-gray-100 text-4xl sm:text-6xl lg:text-6xl leading-none font-extrabold tracking-tight">
+          Nossa História
+        </h1>
+      </motion.div>
+
+      <div>
+        <Paragraph
+          text="A Versátil Cia de Dança foi criada em fevereiro de 1991 por Lucieli Roberta Fernandes Cedran, bailarina, professora e coreógrafa formada em Educação Física pela PUC Campinas, que juntamente com Márcio Cedran dirigiram a academia até o ano de 2013 onde conquistaram o reconhecimento de seu trabalho pela qualidade técnica dos trabalhos apresentados ao longo desses 22 anos de história."
+        />
+        <Paragraph
+          text="Em janeiro de 2014 surgiu a oportunidade de ampliar o trabalho e apostar em um novo desafio, a realização de um sonho, criar um espaço exclusivo para o aprendizado, formação e aperfeiçoamento de diferentes técnicas nas áreas da DANÇA, MÚSICA, ARTES CÊNICAS, CANTO E ARTES PLÁSTICAS, nasce então o CONSERVATÓRIO CAMPINAS com o compromisso de oferecer o que há de melhor em estrutura, metodologia, equipe de profissionais e um ambiente acolhedor onde é priorizado a amizade e a família ."
+        />
+      </div>
+
     </div>
   )
 }
